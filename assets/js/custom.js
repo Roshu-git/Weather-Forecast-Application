f06afe6c59b1104887c86f49258aca5ec6425236
Roshu-git <roshaniadlak15@gmail.com>
2026-06-17 16:38:53 +0530

    toggle button for celcius and fahrenheit working proply done.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -52,7 +52,33 @@ document.getElementById("searchcity").addEventListener("keypress", function(e){
         getCurrentweather();
     }
 })
+// toggle button working celcius and farenheit
+const unitToggle=  document.getElementById("unitToggle");
+unitToggle.addEventListener("change", ()=>{
+    if(unitToggle.checked){
+        currentUnit = "F";
+        console.log("show fareenite");
 
+    }else{
+        currentUnit = "C";
+        console.log("show celcius");
+    }
+    if(weatherData){
+        displayWeather(weatherData); 
+    }
+})
+
+let currentUnit = "C";
+let weatherData = null;
+
+// document.getElementById("celciusbtn").addEventListener("click", function(){
+//     currentUnit ="C"
+//     displayWeather(weatherData);
+// })
+// document.getElementById("fahrenheitbtn").addEventListener("click", function(){
+//     currentUnit ="F"
+//     displayWeather(weatherData);
+// })
 // function when any query occurs 
 async function getWeather(query){
 
@@ -60,6 +86,10 @@ async function getWeather(query){
         const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}`);
         const data = await response.json();
 
+        weatherData = data;
+        const temp = currentUnit === "C" ? data.current.temp_c : data.current.temp_f;
+        const unit = currentUnit === "C" ? "°C" : "°F";
+       
         if(data.error){
             document.getElementById("wf-curweather").innerHTML=`
             <p>${data.error.message} </p>`;
@@ -67,12 +97,40 @@ async function getWeather(query){
         }
 
         document.getElementById("wf-curweather").innerHTML=`
+        <div class="wf-bgimg">
+        <div class="wf-weatdata">
         <h2>${data.location.name}</h2>
-        <p>Temprature: ${data.current.temp_c} / ${data.current.temp_f}</p>
+        <p>Temprature: ${temp} ${unit}</p>
+        <p>Current condition: ${data.current.condition.text}</p>
+        <h2> Chances Of rain:${data.chance_of_rain}</h2>
         <p>Humidity: ${data.current.humidity}</p>
         <p>Wind: ${data.current.wind_kph}</p>
         <img src="${data.current.condition.icon}" />
+        </div></div>
         `
+        const conditiontext = data.current.condition.text.toLowerCase();
+        console.log(conditiontext);
+        const weathercard = document.querySelector(".wf-bgimg");
+        weathercard.classList.remove("sunny", "cloudy", "partly-cloudy" , "rainy", "snowy", "mist","fog","freezing-fog");
+
+        if(conditiontext.includes("cloud") || conditiontext.includes("partly-cloudy") ){
+            weathercard.classList.add("cloudy");
+        } else if(conditiontext.includes("rain")){
+            weathercard.classList.add("rainy");
+        }else if(conditiontext.includes("mist") || conditiontext.includes("fog")){
+            weathercard.classList.add("mist");
+        }else if(conditiontext.includes("snow")){
+            weathercard.classList.add("snowy");
+        }else{
+            weathercard.classList.add("sunny");
+        }
+    
+
+        // console.log(conditiontext == "partly cloudy");
+        // if(conditiontext == "partly cloudy"){
+        //     console.log("partly cloudy called.");
+        //     document.querySelector(".ef-bgimg").style.backgroundImage ="url('../assets/images/partly-cloudy.png')";
+        // }
     }
     catch(error){
         console.log(error);
@@ -91,5 +149,45 @@ async function getCurrentweather(){
     document.querySelector(".error").textContent=" ";
     getWeather(city);
 }
+
+function displayWeather(data){
+    const temp = currentUnit === "C" ? data.current.temp_c : data.current.temp_f ;
+    const unit = currentUnit === "C" ? "°C" : "°F";
+
+    document.getElementById("wf-curweather").innerHTML=`
+     <div class="wf-bgimg">
+        <div class="wf-weatdata">
+        <div class="wf-weatlocation">
+        <img src="./assets/images/location.png">
+        <h2>${data.location.name}</h2>
+        </div>
+        <h2> Chances Of rain:${data.chance_of_rain}</h2>
+        <p>Temprature: ${temp} ${unit}</p>
+         <p>Current condition: ${data.current.condition.text}</p>
+        <p>Humidity: ${data.current.humidity}</p>
+        <p>Wind: ${data.current.wind_kph}</p>
+        <img src="${data.current.condition.icon}" />
+         </div></div>
+        `;
+
+        const conditiontext = data.current.condition.text.toLowerCase();
+        console.log(conditiontext);
+        const weathercard = document.querySelector(".wf-bgimg");
+        weathercard.classList.remove("sunny", "cloudy", "partly-cloudy" , "rainy", "snowy", "mist","fog","freezing-fog");
+
+        if(conditiontext.includes("cloud") || conditiontext.includes("partly-cloudy") ){
+            weathercard.classList.add("cloudy");
+        } else if(conditiontext.includes("rain")){
+            weathercard.classList.add("rainy");
+        }else if(conditiontext.includes("mist") || conditiontext.includes("fog")){
+            weathercard.classList.add("mist");
+        }else if(conditiontext.includes("snow")){
+            weathercard.classList.add("snowy");
+        }else{
+            weathercard.classList.add("sunny");
+        }
+}
+
+// 
 
 
